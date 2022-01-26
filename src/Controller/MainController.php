2a6fe4c13fc576d254fe54a51b6cb5a4725c55bc
Adrien Bodin <AdrienBodin@Hotmail.com>
2022-01-26 15:14:05 +0100

    créer les pages de base pour les séries

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -20,6 +20,14 @@ class MainController extends AbstractController
      */
     public function test()
     {
-        return $this->render('main/test.html.twig');
+        $serie = [
+            "title" => "Games of Thrones",
+            "year" => 2000,
+        ];
+
+        return $this->render('main/test.html.twig', [
+            "mySerie" => $serie,
+            "autreVar" => 4241
+        ]);
     }
 }
